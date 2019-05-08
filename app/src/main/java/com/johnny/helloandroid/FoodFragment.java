484491ac15cd87fd2311b2eb91a4fc18c1a9d957
SCOPUS\jaribeiro <jaribeiro@scopus.com.br>
2019-05-08 14:06:42 -0300

    AlertDialog responding to Activity

---
+++ app/src/main/java/com/johnny/helloandroid/FoodFragment.java
@@ -0,0 +1,52 @@
+package com.johnny.helloandroid;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class FoodFragment extends DialogFragment {
+
+    public static interface Rateable {
+        void modifyRating(String foodName, int amount);
+    }
+
+    private Rateable rater;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        rater = (Rateable) activity;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final String foodName = getArguments().getString("foodName");
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.title_food_rating_dialog).setIcon(android.R.drawable.star_on)
+                .setMessage(String.format("Please provide a rating for %s:", foodName))
+                .setNegativeButton("Negative, -1", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        rater.modifyRating(foodName, -1);
+                    }
+                })
+                .setNeutralButton("Neutral, 0", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Neutral vote, rating remains the same.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setPositiveButton("Positive, +1", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        rater.modifyRating(foodName, 1);
+                    }
+                });
+        return builder.create();
+    }
+}
--- app/src/main/java/com/johnny/helloandroid/FoodQuizActivity.java
+++ app/src/main/java/com/johnny/helloandroid/FoodQuizActivity.java
@@ -1,19 +1,27 @@
 package com.johnny.helloandroid;
 
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.johnny.helloandroid.util.AndroidUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class FoodQuizActivity extends Activity {
+public class FoodQuizActivity extends Activity implements FoodFragment.Rateable {
 
     private ListView listViewFoods;
     private static List<String> foods = new ArrayList<>();
+    private Map<String, Integer> ratings = new HashMap<>();
+    private String foodNameClicked;
 
     static {
         foods.add("Pizza");
@@ -31,9 +39,50 @@ public class FoodQuizActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_quiz);
+        AndroidUtil.displayActionBarBackButton(this);
         ArrayAdapter<String> foodItems = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, foods);
         listViewFoods = findViewById(R.id.lv_foods);
         listViewFoods.setAdapter(foodItems);
-        AndroidUtil.displayActionBarBackButton(this);
+        bindListViewItemClick();
+    }
+
+    private void bindListViewItemClick() {
+        listViewFoods.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                DialogFragment fragment = new FoodFragment();
+                Bundle arguments = new Bundle();
+                foodNameClicked = foods.get(position);
+                arguments.putString("foodName", foodNameClicked);
+                fragment.setArguments(arguments);
+                fragment.show(getFragmentManager(), null);
+            }
+        });
+    }
+
+    @Override
+    public void modifyRating(String foodName, int amount) {
+        if (ratings.get(foodName) != null)
+            ratings.put(foodName, ratings.get(foodName) + amount);
+        else
+            ratings.put(foodName, amount);
+        Toast.makeText(this, String.format("%s has rating of %d", foodName, ratings.get(foodName)), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String[] foodNames = ratings.keySet().toArray(new String[ratings.keySet().size()]);
+        outState.putStringArray("foodNames", foodNames);
+        for (String foodName : foodNames)
+            outState.putInt(foodName, ratings.get(foodName));
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String[] foodNames = savedInstanceState.getStringArray("foodNames");
+        for (String foodName : foodNames)
+            ratings.put(foodName, savedInstanceState.getInt(foodName));
     }
 }
