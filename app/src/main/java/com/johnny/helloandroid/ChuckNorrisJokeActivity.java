deccb7030a60144dca67e9bccf7a6f5789c12cd2
SCOPUS\jaribeiro <jaribeiro@scopus.com.br>
2019-05-21 10:32:22 -0300

    Added "Share Joke" menu option with the use of ShareActionProvider

--- app/src/main/java/com/johnny/helloandroid/ChuckNorrisJokeActivity.java
+++ app/src/main/java/com/johnny/helloandroid/ChuckNorrisJokeActivity.java
@@ -1,11 +1,16 @@
 package com.johnny.helloandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.johnny.helloandroid.service.JokeService;
 import com.johnny.helloandroid.util.AndroidUtil;
@@ -16,6 +21,7 @@ public class ChuckNorrisJokeActivity extends Activity {
     private TextView jokeContainer;
     private JokeService service = new JokeService();
     private AsyncTask<Void, Void, String> task;
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,22 @@ public class ChuckNorrisJokeActivity extends Activity {
         @Override
         protected void onPostExecute(String result) {
             jokeContainer.setText(result);
+            setIntent(result);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.joke_menu, menu);
+        MenuItem item = menu.findItem(R.id.action_share_joke);
+        shareActionProvider = (ShareActionProvider) item.getActionProvider();
+        return true;
+    }
+
+    private void setIntent(String joke){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, joke);
+        shareActionProvider.setShareIntent(intent);
     }
 }
